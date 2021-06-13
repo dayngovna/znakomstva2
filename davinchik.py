23bfe791fa702466435e0b98c1cfdd0ba4ccf7c8
dayngovna <68909674+dayngovna@users.noreply.github.com>
2021-06-13 23:22:47 +0300

    Update davinchik.py

--- davinchik.py
+++ davinchik.py
@@ -3,6 +3,7 @@ import os
 import discord
 from discord.ext import commands
 import pytz
+from random import randint
 client = commands.Bot(command_prefix=">",intents=discord.Intents.all())
 
 @client.event
